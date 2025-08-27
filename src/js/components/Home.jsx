b81cea9b6cdf1d9a8e16edb47f3b0cdca81ffb8e
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-08-27 15:54:40 +0000

    basic functionality complete based on class tutorials

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,31 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+  const [inputValue, setInputValue] = useState("");
 
-	const [inputValue, setInputValue] = useState("");
-	
-	const [chores, setChores] = useState(["go to sleep", "wake up", "dust"]);
+  const [chores, setChores] = useState(["go to sleep", "wake up", "dust"]);
 
-	return (
-		<div className="container">
-		<input
-			type="text"
-			placeholder="Type new task here"
-			onChange={(event) => {
-				const newTask = event.target.value
-				setInputValue(newTask)
-			}}
-		/>
-		<button type="submit">Add task</button>
-		<ul>
-			{chores.map((item, index) => {
-			return <li key={index + "chore"}>{item}</li>;
-			})}
-		</ul>
-		{inputValue}
-		</div>
-	);
+  function addChore(){
+	setChores([...chores, inputValue]);
+  }
+
+  return (
+    <div className="container">
+      <input
+        type="text"
+        placeholder="Type new task here"
+        onChange={(event) => {
+          const newTask = event.target.value;
+          setInputValue(newTask);
+        }}
+      />
+      <button onClick={() => addChore()}>Add task</button>
+      <ul>
+        {chores.map((item, index) => {
+          return <li key={index + "chore"}>{item}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
