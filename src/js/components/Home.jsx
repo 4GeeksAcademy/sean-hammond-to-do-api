a8b69802afbf825e0323b73c8cc3eb1e3269a535
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-08-28 20:33:17 +0000

    formatting

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,50 +1,65 @@
 import React, { useState } from "react";
 
-//TO DO LIST WEB APPLICATION
+// TO DO LIST WEB APPLICATION
 const Home = () => {
-  //New task is empty string until user types and submits it to the tasks array
+  // New task is empty string until user types and submits it to the tasks array
   const [newTask, setNewTask] = useState("");
 
-  //The list of tasks with default hard-coded tasks
+  // The list of tasks with default hard-coded tasks
   const [tasks, setTasks] = useState(["homework", "Dust", "clean room"]);
 
-  //Adds the user's typed task to the array of tasks
+  // Adds the user's typed task to the array of tasks
   function addTask() {
     setTasks([...tasks, newTask]);
   }
 
-  //Delete a specific task
+  // Delete a specific task
   const deleteTask = (taskToDelete) => {
     const filteredTasks = tasks.filter((taskData) => taskData != taskToDelete);
     console.log("Dust:", filteredTasks);
     setTasks(filteredTasks);
   };
 
-  //User can input text in a field and click button or press enter to add typed task to tasks array
+  // User can input text in a field and click button or press enter to add typed task to tasks array
   return (
     <div className="container">
       <input
         type="text"
         placeholder="Type new task here"
-        onChange={(event) => {
-          const typedTask = event.target.value;
-          setNewTask(typedTask);
-        }}
+        onChange={
+          // Whatever is typed becomes newTask
+          (event) => {
+            const typedTask = event.target.value;
+            setNewTask(typedTask);
+          }
+        }
         onKeyDown={(event) => {
+          // Add typed task to list if enter key pressed
           if (event.key == "Enter") {
             addTask();
           }
         }}
       />
-      <button onClick={() => addTask()}>Add task</button>
+      <button
+        onClick={
+          // Add typed task to list if add button clicked
+          () => addTask()
+        }
+      >
+        Add task
+      </button>
       <ul>
-        {tasks.map((item, index) => {
-          return <li key={index + "task"}>{item}</li>;
-        })}
+        {
+          // Print each task as a li
+          tasks.map((item, index) => {
+            return <li key={index + "task"}>{item}</li>;
+          })
+        }
       </ul>
       <button
         onClick={() => {
-          deleteTask();
+          // Delete a specific hard-coded task
+          deleteTask("Dust");
         }}
       >
         Delete
